19b4192590d56f7554e690da93094cd6b10ce526
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-10-06 13:20:36 +0300

    fix: change App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,34 @@
+import { Header } from '@/common/components/Header/Header'
+import { Outlet } from 'react-router-dom'
+import s from './App.module.scss'
+import { useEffect } from 'react'
+import { fetchUsers } from '@/features/users/model/usersSlice'
+import { useAppDispatch } from '@/common/hooks/useAppDispatch'
+import { useSelector } from 'react-redux'
+import { selectIsInitialized } from '@/app/app.selectors'
+import { CircularProgress } from '@/common/components/ui/CircularProgress/CircularProgress'
+import { setAppInitialized } from '@/app/appSlice'
+
 export function App() {
-  return <div>Hello</div>
+  const dispatch = useAppDispatch()
+  const isInitialized = useSelector(selectIsInitialized)
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+      .unwrap()
+      .finally(() => dispatch(setAppInitialized(true)))
+  }, [dispatch])
+
+  if (!isInitialized) return <CircularProgress />
+
+  return (
+    <div>
+      <Header />
+      <main className={s.main}>
+        <div className={s.content}>
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  )
 }
