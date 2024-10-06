13a73969406b21e7244e7587deaa6bf1fb2625c6
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-10-06 09:06:22 +0300

    feat: implemented an adaptive version for the Header component

--- src/common/components/Header/HeaderRight/HeaderRight.tsx
+++ src/common/components/Header/HeaderRight/HeaderRight.tsx
@@ -1,25 +1,19 @@
 import s from './HeaderRight.module.scss'
 import { FavoriteIcon } from '@/assets/icons/FavoriteIcon'
 import { NotificationIcon } from '@/assets/icons/NotificationIcon'
-import userAvatar from '@/assets/images/userAvatar.png'
+import userAvatar from '@/assets/images/userPhoto.jpg'
 import { Typography } from '@/common/components/ui/Typography/Typography'
 
 export const HeaderRight = () => {
   return (
     <div className={s.headerRight}>
       <div className={s.iconsWrapper}>
-        <span>
-          <FavoriteIcon />
-        </span>
-        <span>
-          <NotificationIcon />
-        </span>
+        <FavoriteIcon />
+        <NotificationIcon />
       </div>
       <div className={s.userInfo}>
-        <div className={s.photo}>
-          <img src={userAvatar} alt={'User photo'} />{' '}
-        </div>
-        <Typography as={'span'} variant={'text2'} weight={'medium'}>
+        <img src={userAvatar} alt={'User photo'} className={s.userAvatar} />
+        <Typography as={'span'} variant={'text2'} weight={'medium'} className={s.username}>
           Ivan1234
         </Typography>
       </div>
