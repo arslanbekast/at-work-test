0834e21d2c7430dccf73524915a77bcf9efd9ba4
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-10-06 13:17:16 +0300

    fix: change UserCard component

--- src/features/users/ui/UserCard/UserCard.tsx
+++ src/features/users/ui/UserCard/UserCard.tsx
@@ -1,16 +1,66 @@
 import s from './UserCard.module.scss'
 import { Typography } from '@/common/components/ui/Typography/Typography'
+import { useState } from 'react'
+import { changeUserStatus, hideUser, UserStatus } from '@/features/users/model/usersSlice'
+import { useAppDispatch } from '@/common/hooks/useAppDispatch'
+import { Dropdown } from '@/common/components/ui/Dropdown/Dropdown'
+import { Link } from 'react-router-dom'
 
 type UserCardProps = {
+  id: number
   username: string
   companyName: string
   city: string
   imageUrl: string
+  archive: boolean
 }
 
-export const UserCard = ({ username, companyName, city, imageUrl }: UserCardProps) => {
+export const UserCard = ({ id, username, companyName, city, imageUrl, archive }: UserCardProps) => {
+  const [dropdownShown, setDropdownShown] = useState<boolean>(false)
+
+  const dispatch = useAppDispatch()
+
+  const handleChangeUserStatus = (id: number, status: UserStatus) => {
+    dispatch(changeUserStatus({ id, status }))
+  }
+
+  const handleHideUser = (id: number) => {
+    dispatch(hideUser({ id }))
+  }
+
+  const dropdownItems = archive
+    ? [
+        <button type={'button'} onClick={() => handleChangeUserStatus(id, 'active')}>
+          <Typography
+            as={'span'}
+            variant={'text2'}
+            weight={'medium'}
+            className={s.archiveDropdownItem}
+          >
+            Активировать
+          </Typography>
+        </button>,
+      ]
+    : [
+        <Link to={`/edit/${id}`}>
+          <Typography as={'span'} variant={'text2'} weight={'medium'}>
+            Редактировать
+          </Typography>
+        </Link>,
+        <button type={'button'} onClick={() => handleChangeUserStatus(id, 'archive')}>
+          <Typography as={'span'} variant={'text2'} weight={'medium'}>
+            Архивировать
+          </Typography>
+        </button>,
+        <button type={'button'} onClick={() => handleHideUser(id)}>
+          <Typography as={'span'} variant={'text2'} weight={'medium'}>
+            Скрыть
+          </Typography>
+        </button>,
+      ]
+
   return (
-    <div className={s.userCard}>
+    <div className={`${s.userCard} ${archive && s.archive}`}>
       <div className={s.image}>
         <img src={imageUrl} alt={'User image'} />
       </div>
@@ -19,11 +69,7 @@ export const UserCard = ({ username, companyName, city, imageUrl }: UserCardProp
           <Typography as={'span'} variant={'headline'} weight={'semibold'} className={s.username}>
             {username}
           </Typography>
-          <button type={'button'} className={s.dropdownBtn}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
+          <Dropdown items={dropdownItems} open={dropdownShown} setOpen={setDropdownShown} />
         </div>
         <Typography as={'span'} variant={'text2'} weight={'medium'} className={s.companyName}>
           {companyName}
